632433a360287dd4a19654d02251d9ae53f299ec
nckedev <nmk411@gmail.com>
2021-12-06 22:40:01 +0100

    day 5 start

---
+++ aoc2021/Day5.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2021
+{
+    class Day99 : Problem
+    {
+        public Day99() : base(5)
+        {
+
+        }
+        public override long Solve()
+        {
+            return 0;
+        }
+        public override long Solve2()
+        {
+            return 0;
+        }
+
+    }
+}
+
